7ad24e710c3b13b76153ca9641c5056125657e02
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2024-07-12 18:57:37 +0530

    clean and comp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Hellow from "./components/Hellow";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.google.com/search?q=dhoni&oq=dhoni&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDU2OTJqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          know more about msd
-          
-        
-        </a>
-      </header>
+     <Hellow />
     </div>
   );
 }
